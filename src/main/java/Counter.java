1228b78cf5e1e63932fae95c742b138571004ff6
efe <efemayoglu@gmail.com>
2020-11-02 12:21:45 +0300

    listener added

--- src/main/java/Counter.java
+++ src/main/java/Counter.java
@@ -3,8 +3,13 @@ public class Counter {
 
     private int value;
 
+    private Listener listener;
     public Counter(int initialVaue){
         this.setValue(initialVaue);
+    }
+    public Counter(int initialVaue, Listener listener){
+        this.setValue(initialVaue);
+        this.setListener(listener);
     }
 
     public int getValue() {
@@ -13,5 +18,20 @@ public class Counter {
 
     public void setValue(int value) {
         this.value = value;
+    }
+    public void decrement(){
+        this.setValue(this.getValue()-1);
+    }
+
+    public void register(){
+
+    }
+
+    public Listener getListener() {
+        return listener;
+    }
+
+    public void setListener(Listener listener) {
+        this.listener = listener;
     }
 }
--- src/test/java/CounterTest.java
+++ src/test/java/CounterTest.java
@@ -1,18 +1,62 @@
 import static org.junit.jupiter.api.Assertions.*;
 
 class CounterTest {
-
     @org.junit.jupiter.api.Test
+
     public void testCreation(){
-        Counter dog = new Counter(0);
+        Counter dog = new Counter(60);
         assertNotNull(dog);
     }
     @org.junit.jupiter.api.Test
+
     public void testGetValue(){
         int initialValue = 30;
         Counter dog = new Counter(initialValue);
         int value = dog.getValue();
         assertEquals(initialValue, value);
+
+    }
+
+    @org.junit.jupiter.api.Test
+
+    public void testDecrement(){
+        int initialValue = 60;
+        Counter dog = new Counter(initialValue);
+        for (int i=1; i<initialValue;i++){
+            dog.decrement();
+            int value = dog.getValue();
+            assertEquals(initialValue-i, value);
+        }
+        int finalValue = dog.getValue();
+        assertEquals(1, finalValue);
+
+
+    }
+
+    @org.junit.jupiter.api.Test
+    public void testNotitfy(){
+        int initialValue = 60;
+        Counter dog = new Counter(initialValue);
+        int value = dog.getValue();
+
+        while (value >0){
+            dog.decrement();
+            value = dog.getValue();
+        }
+        int finalValue = dog.getValue();
+        assertEquals(0, finalValue);
+
+
+    }
+
+
+
+    @org.junit.jupiter.api.Test
+    public void testRegistaration(){
+        Listener myListener = new Listener();
+        int initialValue = 60;
+        Counter dog = new Counter(initialValue);
+       dog.register(myListener);
 
     }
 }
